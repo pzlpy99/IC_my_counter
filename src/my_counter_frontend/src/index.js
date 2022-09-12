2c585548c98637da5d9b218030000ccbe950b71e
peizhi <peizhi@bytedance.com>
2022-09-12 12:18:19 +0800

    Initial commit.

---
+++ src/my_counter_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { my_counter_backend } from "../../declarations/my_counter_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await my_counter_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
